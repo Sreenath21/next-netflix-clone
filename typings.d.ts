f536aef305aa16301e9ed4843363213f9c275d12
Sreenath Paramasivam <psreenath0201@gmail.com>
2022-09-19 15:03:11 +0530

    day-1

---
+++ typings.d.ts
@@ -0,0 +1,32 @@
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_data: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+  media_type?: string;
+}
+
+export interface Element {
+  type:
+    | "Bloopers"
+    | "Featurette"
+    | "Behind the scenes"
+    | "CLip"
+    | "Trailer"
+    | "Teaser";
+}
